5bf3274c92264810ab195b9313a8f7b8c1270d46
Marcus Farias <marcuscfarias@outlook.com>
2026-08-17 10:03:41 +0200

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.cs
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode newList = new ListNode(0);
+        ListNode curr = newList;
+
+        while(list1 != null && list2 != null){
+
+            if(list1.val >= list2.val){
+
+                curr.next = list2;
+                list2 = list2.next;
+            }else{ 
+
+                curr.next = list1;
+                list1 = list1.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = list1 ?? list2;
+
+        return newList.next;
+    }
+}
