f15329e61823a96bdc41423778bd94fa741d66bf
Marcus Farias <marcuscfarias@outlook.com>
2026-08-21 13:18:53 +0200

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int DiameterOfBinaryTree(TreeNode root) {
+        
+        int diameter = 0;
+
+        BinaryTreeHeight(root, ref diameter);
+
+        return diameter;
+    }
+
+
+    private int BinaryTreeHeight(TreeNode root, ref int diameter){
+
+        if(root == null){
+            return 0;
+        }
+
+        int left = BinaryTreeHeight(root.left, ref diameter);
+        int right = BinaryTreeHeight(root.right, ref diameter);
+
+        int height = 1 + Math.Max(left, right);
+        diameter = Math.Max(diameter, left + right);
+
+        Console.WriteLine("Root: {0}, Height: {1}, Left: {2}, Right: {3}, Diameter: {4}", root.val, height, left, right, diameter);
+
+        return height;
+    }
+}
